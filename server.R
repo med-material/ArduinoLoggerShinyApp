9df163bcc6bd22b4bf916d6204657ff157956e56
Bastian Ilso <contact@bastianilso.com>
2020-02-05 08:54:09 +0100

    server.R: refactor input$test to input$emailSelect
    Refactors input$test to input$emailSelect.
    Furthermore, the variable "pal" is now colorPalette and
    is only defined a single time, instead of multiple times.

--- server.R
+++ server.R
@@ -2,24 +2,27 @@ library(plyr)
 
 server = function(input, output, session) {
   
+  # define colors to use in plots.
+  colorPalette <- c("#c94232","#239a37")
+  
   observeEvent({input$tabs},{
     print(input$tabs)
     if(input$tabs == "<strong>ReactionTime</strong>"){
-      updateSelectInput(session , "test", choices = GenerateSelectChoices(default = "All test", text = "", fieldName = "Email" , tablename = "v_reactiontime"))
+      updateSelectInput(session , "emailSelect", choices = GenerateSelectChoices(default = "All test", text = "", fieldName = "Email" , tablename = "v_reactiontime"))
      
     }
     else if(input$tabs == "<strong>SynchTime</strong>"){
-      updateSelectInput(session , "test", choices = GenerateSelectChoices(default = "All test", text = "", fieldName = "Email", tablename = "synch"))
+      updateSelectInput(session , "emailSelect", choices = GenerateSelectChoices(default = "All test", text = "", fieldName = "Email", tablename = "synch"))
     }
-    observeEvent({input$test} ,{
-      if (input$test != -1){
-      varparam = list(list(paste("Email = '", input$test,"'", sep = "")))
+    observeEvent({input$emailSelect} ,{
+      if (input$emailSelect != -1){
+      varparam = list(list(paste("Email = '", input$emailSelect,"'", sep = "")))
       updateSelectInput(session , "Param", choices = GenerateSelectChoices(default = "All test", text = "", fieldName = "Comment", tablename = "synch", conditions = varparam))
       show(id = "synchAlldatacheck", anim = TRUE, animType = "slide", time = 0.5,
            selector = NULL)
       updateCheckboxInput(session, "synchAlldatacheck", value = FALSE)
       }
-      else if (input$test == -1) {
+      else if (input$emailSelect == -1) {
         updateSelectInput(session , "Param", choices = GenerateSelectChoices(default = "All test", text = "", fieldName = "Comment", tablename = "synch"))
         hide(id = "synchAlldatacheck", anim = TRUE, animType = "slide", time = 0.5,
              selector = NULL)
@@ -29,7 +32,7 @@ server = function(input, output, session) {
     })
   
   
-  observeEvent({input$test},{
+  observeEvent({input$emailSelect},{
     
   observeEvent(input$visual,{
     
@@ -41,10 +44,10 @@ server = function(input, output, session) {
       var<-list()
       varbound<-list()
       varmodal<-list()
-      if (input$test != -1){
-        var = list(list(paste("Email = '", input$test,"'", sep = "")))
-        #varbound = list(list(paste("Email = '", input$test,"'", sep = "")))
-        varmodal = list(list(paste("Email = '", input$test,"'"," AND Modal = 'ERM'", sep = "")))
+      if (input$emailSelect != -1){
+        var = list(list(paste("Email = '", input$emailSelect,"'", sep = "")))
+        #varbound = list(list(paste("Email = '", input$emailSelect,"'", sep = "")))
+        varmodal = list(list(paste("Email = '", input$emailSelect,"'"," AND Modal = 'ERM'", sep = "")))
         enable(id = "Alldatacheck")
         
       }
@@ -53,24 +56,21 @@ server = function(input, output, session) {
         updateCheckboxInput(session, "Alldatacheck", value = FALSE)
         }
       
-      pal <- c("#c94232","#239a37")
       reactiondata = FetchDatas(conditionLists= var , option = "TimeStamp, Email, TrialNo, ReactionTime, Modal", tablename = "v_reactiontime")
       reactiondata$ReactionTimeRounded = round(reactiondata$ReactionTime, digits=-1)
 
                                    
-                                   if(input$Alldatacheck){
-                                     output$movementTimePlot <- renderPlotly(plot_ly(reactiondata, x = ~reactiondata$TrialNo, y = ~reactiondata$ReactionTimeRounded)%>% 
-                                                                  add_trace(type = 'scatter', mode='lines+markers', name = ~Modal , color = ~Modal , colors = pal, split = ~TimeStamp)%>%
-                                                                  add_trace(data = varbound, type = 'scatter', mode='lines',fill = 'tonexty',fillcolor='rgba(0,100,80,0.2)', line = list(color = 'transparent'), color = "grey")%>%
-                                                                  layout(showlegend = FALSE, xaxis = list(title = "Trial Number"), yaxis = list(title = "Reaction Time (ms)")))
-                                      
-                                   }
-      
-      
-      else {
-        output$movementTimePlot <- renderPlotly(plot_ly(reactiondata, x = ~reactiondata$TrialNo, y = ~reactiondata$ReactionTimeRounded)%>% 
-                                       add_trace(type = 'scatter', mode='lines+markers', line = list(width = 1.5), name = ~Modal , color = ~Modal , colors = pal, split = ~TimeStamp)%>%
-                                      layout(showlegend = FALSE, xaxis = list(title = "Trial Number"), yaxis = list(title = "Reaction Time (ms)")))
+     if(input$Alldatacheck){
+       output$movementTimePlot <- renderPlotly(plot_ly(reactiondata, x = ~reactiondata$TrialNo, y = ~reactiondata$ReactionTimeRounded)%>% 
+        add_trace(type = 'scatter', mode='lines+markers', name = ~Modal , color = ~Modal , colors = colorPalette, split = ~TimeStamp)%>%
+        add_trace(data = varbound, type = 'scatter', mode='lines',fill = 'tonexty',fillcolor='rgba(0,100,80,0.2)', line = list(color = 'transparent'), color = "grey")%>%
+        layout(showlegend = FALSE, xaxis = list(title = "Trial Number"), yaxis = list(title = "Reaction Time (ms)")))
+        
+     }
+     else {
+      output$movementTimePlot <- renderPlotly(plot_ly(reactiondata, x = ~reactiondata$TrialNo, y = ~reactiondata$ReactionTimeRounded)%>% 
+       add_trace(type = 'scatter', mode='lines+markers', line = list(width = 1.5), name = ~Modal , color = ~Modal , colors = colorPalette, split = ~TimeStamp)%>%
+      layout(showlegend = FALSE, xaxis = list(title = "Trial Number"), yaxis = list(title = "Reaction Time (ms)")))
       }
       
     })
@@ -80,20 +80,19 @@ server = function(input, output, session) {
     if(input$visual == "radio2"){
       hide(id = "Alldatacheck", anim = TRUE, animType = "slide", time = 0.5,
            selector = NULL)
-      observeEvent({input$test},{
+      observeEvent({input$emailSelect},{
         var<-list()
         varmodal<-list()
-        if (input$test != -1){
-          var = list(list(paste("Email = '", input$test, "'", sep = "")))
-          varmodal = list(list(paste("Email = '", input$test,"'"," AND Modal = 'ERM'", sep = "")))
+        if (input$emailSelect != -1){
+          var = list(list(paste("Email = '", input$emailSelect, "'", sep = "")))
+          varmodal = list(list(paste("Email = '", input$emailSelect,"'"," AND Modal = 'ERM'", sep = "")))
         }
         
-        pal <- c("red","green")
         reactiondataintens = FetchDatas(conditionLists = var,option = "TimeStamp, Email, Intens, ReactionTime, Modal", tablename = "v_reactiontime")
         reactiondataintens$ReactionTimeRounded = round(reactiondataintens$ReactionTime, digits=-1)
         
         output$plot1 <- renderPlotly(plot_ly(reactiondataintens ,x = ~reactiondataintens$Intens, y = ~ reactiondataintens$ReactionTimeRounded)%>% 
-                                    add_trace(type = 'scatter', mode='markers', name = ~Modal,  color = ~Modal , colors = pal,split = ~TimeStamp)%>%
+                                    add_trace(type = 'scatter', mode='markers', name = ~Modal,  color = ~Modal , colors = colorPalette,split = ~TimeStamp)%>%
                                     layout(showlegend = FALSE, xaxis = list(title = "Intensity"), yaxis = list(title = "Reaction Time (ms)")))          
         
       })
@@ -104,17 +103,16 @@ observeEvent(input$Param, {
   observeEvent(input$showled,{
     observeEvent(input$showerm, {
     #plot if led is checked
-    if (input$test != -1){
-      var1<-contage[contage$synchdataLED.Email == input$test & contage$synchdataLED.MusicalAbility == 'High',]
-      var2<-contage[contage$synchdataLED.Email == input$test & contage$synchdataLED.MusicalAbility == 'Low',]
+    if (input$emailSelect != -1){
+      var1<-contage[contage$synchdataLED.Email == input$emailSelect & contage$synchdataLED.MusicalAbility == 'High',]
+      var2<-contage[contage$synchdataLED.Email == input$emailSelect & contage$synchdataLED.MusicalAbility == 'Low',]
 
-      pal <- c("red","green")
 
       output$plot2 <- renderPlotly(plot_ly(contage,x = ~var1$synchdataLED.ReactionTimeRounded, y = ~var1$freq)%>%
-                                     add_trace(type = 'bar',name = ~var1$synchdataLED.Modal,  color = ~var1$synchdataLED.Modal , colors = pal)%>% 
+                                     add_trace(type = 'bar',name = ~var1$synchdataLED.Modal,  color = ~var1$synchdataLED.Modal , colors = colorPalette)%>% 
                                     layout(title = "Has MusicalAbility", xaxis = list(title = "Reaction Time (ms)"), yaxis = list(title = "Diversity")))
       output$plot3 <- renderPlotly(plot_ly(contage,x = ~var2$synchdataLED.ReactionTimeRounded, y = ~var2$freq)%>%
-                                     add_trace(type = 'bar',name = ~var2$synchdataLED.Modal,color = ~var2$synchdataLED.Modal , colors = pal)%>% 
+                                     add_trace(type = 'bar',name = ~var2$synchdataLED.Modal,color = ~var2$synchdataLED.Modal , colors = colorPalette) %>% 
                                      layout(title = "Has No MusicalAbility", xaxis = list(title = "Reaction Time (ms)"), yaxis = list(title = "Diversity")))
     }
     
@@ -122,13 +120,12 @@ observeEvent(input$Param, {
       var1<-contage[contage$synchdataLED.MusicalAbility == 'High',]
       var2<-contage[contage$synchdataLED.MusicalAbility == 'Low',]
   
-      pal <- c("red","green")
 
       output$plot2 <- renderPlotly(plot_ly(contage,x = ~var1$synchdataLED.ReactionTimeRounded, y = ~var1$freq)%>%
-                                     add_trace(type = 'bar',name = ~var1$synchdataLED.Modal,color = ~var1$synchdataLED.Modal , colors = pal)%>% 
+                                     add_trace(type = 'bar',name = ~var1$synchdataLED.Modal,color = ~var1$synchdataLED.Modal , colors = colorPalette)%>% 
                                   layout(title = "Has MusicalAbility", xaxis = list(title = "Reaction Time (ms)"), yaxis = list(title = "Diversity")))
       output$plot3 <- renderPlotly(plot_ly(contage,x = ~var2$synchdataLED.ReactionTimeRounded, y = ~var2$freq)%>%
-                                     add_trace(type = 'bar',name = ~var2$synchdataLED.Modal,color = ~var2$synchdataLED.Modal , colors = pal)%>% 
+                                     add_trace(type = 'bar',name = ~var2$synchdataLED.Modal,color = ~var2$synchdataLED.Modal , colors = colorPalette)%>% 
                                      layout(title = "Has No MusicalAbility", xaxis = list(title = "Reaction Time (ms)"), yaxis = list(title = "Diversity")))
     }
     
@@ -136,12 +133,11 @@ observeEvent(input$Param, {
       var1<-contage[contage$synchdataLED.MusicalAbility == 'High' & contage$synchdataLED.Comment == input$Param,]
       var2<-contage[contage$synchdataLED.MusicalAbility == 'Low' & contage$synchdataLED.Comment == input$Param,]
 
-      pal <- c("red","green")
       output$plot2 <- renderPlotly(plot_ly(contage,x = ~var1$synchdataLED.ReactionTimeRounded, y = ~var1$freq)%>%
-                                     add_trace(type = 'bar',name = ~var1$synchdataLED.Modal,color = ~var1$synchdataLED.Modal , colors = pal)%>% 
+                                     add_trace(type = 'bar',name = ~var1$synchdataLED.Modal,color = ~var1$synchdataLED.Modal , colors = colorPalette)%>% 
                                      layout(title = "Has MusicalAbility", xaxis = list(title = "Reaction Time (ms)"), yaxis = list(title = "Diversity")))
       output$plot3 <- renderPlotly(plot_ly(contage,x = ~var2$synchdataLED.ReactionTimeRounded, y = ~var2$freq)%>%
-                                     add_trace(type = 'bar',name = ~var2$synchdataLED.Modal,color = ~var2$synchdataLED.Modal , colors = pal)%>% 
+                                     add_trace(type = 'bar',name = ~var2$synchdataLED.Modal,color = ~var2$synchdataLED.Modal , colors = colorPalette)%>% 
                                      layout(title = "Has No MusicalAbility", xaxis = list(title = "Reaction Time (ms)"), yaxis = list(title = "Diversity")))
     } 
 })
